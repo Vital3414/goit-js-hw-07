24fd00719ac7eba7de7aec4b94b1e2e9d7f3a8c6
Vital3414 <qwer1234432148@gmail.com>
2023-03-30 15:01:40 +0300

    homework7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,53 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+galleryRef.insertAdjacentHTML(
+    "afterbegin",
+    createGalleryMarkupFromArray(galleryItems)
+);
+
+function createGalleryMarkupFromArray(itemArray) {
+    return itemArray.map((item) => {
+        return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+    <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+    />
+    </a>
+</div>`;
+    })
+        .join("");
+}
+
+function onGalleryItemClick(event) {
+    event.preventDefault();
+
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+
+    const modal = window.basicLightbox.create(`
+    <img src="${event.target.dataset.source}" alt="${event.target.alt}">
+`, {
+        onShow: () => { document.addEventListener('keydown', onKeyEscapeClick) },
+        onClose: () => { document.removeEventListener('keydown', onKeyEscapeClick) },
+    });
+
+    modal.show();
+
+    function onKeyEscapeClick(event) {
+        event.stopPropagation();
+        // console.log(event.code);
+        if (event.code === "Escape") {
+            modal.close();
+        }
+        return;
+    };
+};
